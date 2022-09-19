a4b3259eb7d5f169a15a40ee45a08c67eff0026f
altnskmuhammed <altnskm@outlook.com>
2022-09-19 23:58:40 +0300

    Create exercise9.js

---
+++ exercise-9/exercise9.js
@@ -0,0 +1,16 @@
+const firstName = "Mario";
+const lastname = "Rossi";
+const age = 27;
+const isGraduated = false;
+const average = 27;
+const examsCompleted = 10;
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+console.log(firstName == lastname);
+console.log(age <= average);
+console.log(firstYearCompleted == lastname);
+console.log(yearsCompleted == firstYearCompleted);
+console.log(firstYearCompleted === yearsCompleted);
+console.log(examsCompleted < age);
+console.log(isGraduated > yearsCompleted);
