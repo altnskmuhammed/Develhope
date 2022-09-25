05cf0119ff27e9e979b121546a1a5be4737652d3
altnskmuhammed <altnskmuh23@gmail.com>
2022-09-25 17:52:45 +0300

    Create exercise23.js

---
+++ exercise-23/exercise23.js
@@ -0,0 +1,16 @@
+let roomType1 = "Basic Room";
+let roomType2 = "Junior Suite Room";
+let roomType3 = "Master Suite";
+
+switch (roomType1) {
+  case "Basic Room":
+    console.log("Price for " + roomType1 + "50 Euro");
+    break;
+
+  case "Junior Suite Room":
+    console.log("Price for " + roomType2 + "80 Euro");
+    break;
+  case "Master Suite":
+    console.log("Price for " + roomType3 + "100 Euro");
+    break;
+}
