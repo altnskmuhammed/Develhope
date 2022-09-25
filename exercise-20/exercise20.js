d31306c2c9a351ee25ad22c2f2e35532946a211e
altnskmuhammed <altnskmuh23@gmail.com>
2022-09-25 08:51:24 +0300

    Create exercise20.js

---
+++ exercise-20/exercise20.js
@@ -0,0 +1,13 @@
+let name = "muhammed";
+let num = 25;
+let bool = true;
+
+if (typeof name === String) {
+  console.log(name + "string");
+}
+if (typeof num === Number) {
+  console.log(num + "Number");
+}
+if (typeof bool === Boolean) {
+  console.log(bool + "boolean");
+}
