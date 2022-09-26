d1da429d89f25850cfdb41b460616c6d5a8288d4
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-09-26 22:26:58 +0300

    Create exercise35.js

---
+++ exercise-35/exercise35.js
@@ -0,0 +1,17 @@
+
+
+
+function canPlay() {
+  var sport = " Football";
+
+if (true) { 
+  var personName = "Cosimo"; 
+}
+
+console.log(personName + sport); }
+
+canPlay()
+
+
+
+// person name is not defined because we call person name from outsıde canPlay() scope  for fix that i change to be var key words
