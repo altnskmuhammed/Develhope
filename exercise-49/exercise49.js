d05f3c1dc25fa28adf9e8458ea5b36de5bacee99
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-09-28 23:53:33 +0300

    Create exercise49.js

---
+++ exercise-49/exercise49.js
@@ -0,0 +1,7 @@
+const students = [
+  { id: 1, name: "Luca", surname: "Rossi", age: 20 },
+  { id: 2, name: "Mario", surname: "Bianchi", age: 24 },
+  { id: 3, name: "Giovanni", surname: "Brambilla", age: 30 },
+];
+
+console.log(Object.entries(students));
