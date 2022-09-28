fb9df0460a6f7f8288eb7359f859cb91bdafc1b1
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-09-28 23:44:02 +0300

    Create exercise47.js

---
+++ exercise-47/exercise47.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers[0]);
+console.log(numbers[numbers.length - 1]);
+console.log(numbers.length);
+console.log(numbers[20]); // undifined because we dont define 20. item
