fb3b33ff4f9d0800c124916d40e5916074ec0656
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-10-04 17:55:31 +0300

    Create exercise53.js

---
+++ exercise-53/exercise53.js
@@ -0,0 +1,20 @@
+const persons = [
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
+];
+function adultFilter(persons) {
+  return persons.age > 30;
+}
+const adults = persons.filter((old) => {
+  return old.age > 30;
+});
+
+console.log(adults);
