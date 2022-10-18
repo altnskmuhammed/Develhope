7dee6e5a36a056ae5496e8e743de5dd00361b49c
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-10-18 15:23:13 +0300

    Update exercise19.js

--- exercise-19/exercise19.js
+++ exercise-19/exercise19.js
@@ -1,7 +1,7 @@
 let name = "muhammed";
 let surname = "Altunışık";
 
-if (name == "muhammed" && surname == "Altunışık") {
+if (name === "muhammed" && surname === "Altunışık") {
   console.log("trutly");
 } else {
   console.log("invalid");
